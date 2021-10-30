8176da7520fa2bae8649049ba06fda1172308bd1
dbrown271 <d.brown271@icloud.com>
2021-10-30 11:18:46 -0500

    birthday button added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <ClassComponent lastName = {"Doe"}firstName = {"Jane"} age = {"45"} hairColor = {"Black"}/>
-      <ClassComponent lastName = {"Smith"}firstName = {"John"} age = {"88"} hairColor = {"Brown"}/>
-      <ClassComponent lastName = {"Fillmore"}firstName = {"Millard"} age = {"50"} hairColor = {"Brown"}/>
-      <ClassComponent lastName = {"Smith"}firstName = {"Maria"} age = {"62"} hairColor = {"Brown"}/>
+      <ClassComponent lastName = {"Doe"}firstName = {"Jane"} age = {45} hairColor = {"Black"}/>
+      <ClassComponent lastName = {"Smith"}firstName = {"John"} age = {88} hairColor = {"Brown"}/>
+      <ClassComponent lastName = {"Fillmore"}firstName = {"Millard"} age = {50} hairColor = {"Brown"}/>
+      <ClassComponent lastName = {"Smith"}firstName = {"Maria"} age = {62} hairColor = {"Brown"}/>
     </div>
   );
 }
--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 
 class ClassComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age : this.props.age
+        };
+    }
+
     render() {
+        
+        let birthdayClick = () => {
+            console.log("You Clicked")
+            // alert("You're Getting Some Where!")
+            // console.log("Changing The Age", age)
+            this.setState({age: this.state.age +1})
+            // console.log("New Age", age)
+        }
         return(
             <div>
                 <h1>Name: {this.props.lastName}, {this.props.firstName} </h1>
-                <p>Age: {this.props.age} </p>
+                <p>Age: {this.state.age} </p>
                 <p>Hair Color: {this.props.hairColor} </p>
-                
+                <button onClick = {birthdayClick}>Birthday Button For {this.props.firstName} {this.props.lastName} </button>
+                <hr />
             </div>
         )
     }
